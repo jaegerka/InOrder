5b65a4c4e5f63a6142e2a7b39e25665083334c4d
Kaleigh Jaeger <kjaeger123@gmail.com>
2019-10-04 16:07:53 -0500

    Changes to Services and Summary

---
+++ resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (firstname, lastname, username, position) {
+    VALUES('Kaleigh', 'Jaeger', 'kjaeger123', 'associate'),
+    VALUES('Arvester', 'Dickerson', 'adickerson224', 'associate'),
+    VALUES('Moises', 'Trevino', 'mtrev396', 'unassigned'),
+    VALUES('Emily', 'Higgins', 'ehiggs101', 'trainer'),
+    VALUES('Darius', 'Watts', 'dwatts43', 'scrummaster')
+}
---
+++ src/main/java/InOrder/InOrderMain.java
@@ -0,0 +1,10 @@
+package InOrder;
+
+public class InOrderMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
